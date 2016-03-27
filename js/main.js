6d72124920249d0d15bdbd67ce9454026dd015a9
heavydott <shustart@gmail.com>
2016-03-28 00:14:49 +0300

    1.0

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+/**
+ * JSON
+ */
+function fetchJSONFile(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send();
+}
+
+// this requests the file and executes a callback with the parsed result once
+//   it is available
+fetchJSONFile('/resources/data.json', function (data) {
+    // do something with your data
+
+    for (var i = 0; i < data.cars.length; i++) {
+
+    }
+
+    console.log(data);
+});
+
+/**
+ * LEAFLET
+ */
 var map = L.map('map').setView([53.8999, 27.5566], 12);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {
@@ -11,3 +43,4 @@ marker1.bindPopup('<b>Yo-yo</b>');
 marker1.on('click', function (e) {
     marker1.getPopup().setContent('<b>Yo-yo</b>');
 })
+
